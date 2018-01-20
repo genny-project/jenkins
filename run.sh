a394f2c9f92739e215902aa053df5e999559021c
DomHynes <DominicHynes@gmail.com>
2018-01-20 18:29:01 +1100

    updated jenkins

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-docker run \
+docker run -d \
   -p 8080:8080 -p 50000:50000 \
-  -v jenkins_home:/var/jenkins_home  \
+  -v /var/jenkins_home:/var/jenkins_home  \
   -v /var/run/docker.sock:/var/run/docker.sock \
   gennyproject/jenkins:latest
